fcc96ab676cef2f60831081f268e2917df1992dc
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-09-01 17:53:00 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-gray-700 text-orange-500 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
