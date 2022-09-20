505b28dd04446d7bfd332883727c2cabc91d9ef5
Zakaria Abdessamed Brahimi <brahimi.zakaria.abdessamed@gmail.com>
2022-09-20 13:53:51 +0100

    Update settings

--- backend/project/settings/production.py
+++ backend/project/settings/production.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = ['*'] #TODO: add the allowed host
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL_HOST_USER") #TODO: Change the email and password
+EMAIL_HOST_USER = env("EMAIL_HOST_USER") #TODO: Change the email and password.
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
